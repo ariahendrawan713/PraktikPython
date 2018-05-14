d30be48c81563ff9819ec1202328584c5390fdf4
muhmadsigitpranata <38850542+muhmadsigitpranata@users.noreply.github.com>
2018-05-14 15:39:52 +0700

    laporan1.py

---
+++ laporan1.py
@@ -0,0 +1,3 @@
+Hasil dari percobaan saya mengenai script1, script2, dan string. saat melakukan percobaan di ubuntu virtual box
+antara script1, script2 dan string meskipun ada perbedaan script yaitu pada penggunaan tanda petik, antara penggunaan tanda petik 1 dan petik 2
+tidak menunjukkan perbedaan sama sekali.
